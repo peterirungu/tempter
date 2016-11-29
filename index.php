57e46c83ded5b081ee4a5b7eede31cee78fcde35
peterirungu <peterirungu16@gmail.com>
2016-11-29 18:00:41 +0300

    styled three pages

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Tempter</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/tempter.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- <a class="navbar-brand" href="index.php">Tempter</a> -->
+                <a href="index.php" class="brand-logo"><img src="images/logo.png" height="100px" alt="Tempter"></a> 
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="near-you.php">Near You</a></li>
+                    <li><a href="view-match.php">Matches</a></li>
+                    <li><a href="#">Get Escort</a></li>
+                    <li><a href="#">Recent Activity</a></li>
+                    <li class="nav  navbar-right"><a href="#"><img src="images/g1.png" height="31px" alt="Tempter"></a>
+                        <!-- <ul>
+                            <li><a href="#">Link 1</a></li>
+                            <li><a href="#">Link 2</a></li>
+                            <li><a href="#">Link 3</a></li>
+                        </ul> -->
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="business-header">
+    <img src="images/b1.jpg" alt="" width="100%" height="290px">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <!-- The background image is set in the custom CSS -->
+                    <h1 class="tagline">
+                    <a href="#"><img src="images/order.png" alt="" width="280px" height="50px" class="img-responsive"></a>
+                    <!-- <button class="btn btn-tag">ORDER ESCORT</button> -->
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <p style="color:#662200; font-weight:bold;">Online Now</p>
+
+        <hr style="color:#662200;">
+
+        <div class="row">
+            <!-- Page Features -->
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g1.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                     
+                        <p>
+                      <!--      <div class="clear-fix pull-left" > -->
+                            <ul>
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        <!-- </div> -->
+                        </p>                    
+                         <p> </p> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g2.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                     
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p>
+                         <p> </p> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g3.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                     
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p>
+                        <p> </p> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g4.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                     
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p>
+                        <p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g5.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                
+                        <p>Nairobi Nairobi Kenya</p>                      
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g6.jpg" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                      
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p><p> </p>      
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/G7.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                      
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <img src="images/g8.png" alt="" width="350px" height="200px">
+                    <div class="caption">
+                        <h5>Lorem ipsum - 21</h5>                 
+                        <p>Nairobi Nairobi Kenya</p>                     
+                        <p>
+                           <div >
+                            <ul class="">
+                                <a href="#"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></a>
+                                <a href="#"><i class="fa fa-camera fa-2x" aria-hidden="true"></i></a>
+                            </ul>
+                        </div>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <!-- /.row -->
+        </div>
+        <div class="row text-center">
+            <div class="col-lg-12">
+               <button class="btn-premium"><span class="fa fa-asterisk" aria-hidden="true"></span> Become a Premium Member</button>
+            </div>
+        </div>
+        
+        <footer>
+            <div class="row text-center">
+                <div class="col-lg-12">
+                   <p>Copyright &copy; Tempter 2016 | Terms &amp Condations</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /container -->
+
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
